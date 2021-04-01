b85abd14537f9d723ab260dc379af3f75e2994cb
woyuen <wing-on@stelace.com>
2021-04-01 20:29:06 +0200

    fix: exported commonjs function doesn't work anymore
    Due to @babel/preset-env bump, ES6 code cannot import
    commonjs exported function

--- src/utils/stelace.js
+++ src/utils/stelace.js
@@ -1,7 +1,9 @@
-import { initStelaceSdk } from './init-sdk'
+import { createInstance } from 'stelace'
 
 const apiBaseURL = process.env.STELACE_API_URL
 const apiKey = process.env.STELACE_PUBLISHABLE_API_KEY
+
+const apiVersion = '2019-05-20'
 
 const MAX_RESULTS_PER_PAGE = 100 // cf. Stelace API reference for pagination details
 
@@ -38,4 +40,20 @@ async function fetchAllResults (fetchFn, params = {}) {
 
 function getStelaceEnv () {
   return apiKey.includes('_live_') ? 'live' : 'test'
+}
+
+export function initStelaceSdk ({ apiBaseURL, apiKey }) {
+  const stelace = createInstance({ apiKey, apiVersion })
+
+  if (apiBaseURL) {
+    const parsedUrl = new URL(apiBaseURL)
+
+    const host = parsedUrl.hostname
+    const port = parsedUrl.port
+    const protocol = parsedUrl.protocol.slice(0, -1)
+
+    stelace.setHost(host, port, protocol)
+  }
+
+  return stelace
 }
